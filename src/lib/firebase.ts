e49b7fcee18dbbc6c4f4e3cd551e66a4b78f9213
Ubuntu Academy <kishansan28@gmail.com>
2025-09-05 07:43:26 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "mindfiti",
+  "appId": "1:964015669970:web:9ac647fceb1753b69be30b",
+  "storageBucket": "mindfiti.firebasestorage.app",
+  "apiKey": "AIzaSyB3ZfB_VpQKCQXUdX4eEwVlWSGQpvpy2vc",
+  "authDomain": "mindfiti.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "964015669970"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
